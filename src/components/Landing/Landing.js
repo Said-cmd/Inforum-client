6ae411c58992a0627a0b4f5b602c2b13050370ac
Said Hussein <60461223+Said-cmd@users.noreply.github.com>
2022-10-19 21:57:41 +0300

    Update Landing.js

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,30 +1,21 @@
-import { GiNotebook } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import "../Landing/Landing.css";
+import { GiNotebook } from "react-icons/gi";
 
 const Landing = ({ user }) => {
     return (
         <div className="landing">
         <div className="section-holder">
         <div className="text">
-        <h1>Follow the ideas from your favourite topics.</h1>
-        <h1>Participate in the discourse.</h1>
-        <h1>Give your ideas a voice.</h1>
-        </div>
-        <div className="brand-logo">
-        <GiNotebook className="logo" color="green" size="25rem"/>
-        </div>
-        </div>
+        <br></br>
+        <h2 className="h2-landing">Give your ideas a voice.</h2>
+        <br></br>
+        <h3 className="h3-landing">Follow the ideas from your favourite topics <br></br> and participate in the discourse.</h3>
         { user ? <Link to="/blogs"><input className="start-reading" type="button" value="Back to blogs"/></Link> : (
         <Link to="/signup"><input className="start-reading" type="button" value="Start reading"/></Link>
         )}
-        <div className="footer">
-        <div className="footer-content">
-        <p style={{ color: "green"}}>© 2022 Inforum. All rights reserved.</p>
-        <p style={{ color: "green"}}>Terms of use</p>
-        <p style={{ color: "green"}}>FAQ</p>
-        <p style={{ color: "green"}}>Privacy Policy</p>
         </div>
+        <GiNotebook className="landing-logo" size="480px"/>
         </div>
         </div>
     );
